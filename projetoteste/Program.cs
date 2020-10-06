970635883df38dab87020e0a6e2821163bac447e
Pedro Pontes <pedrorpontes@gmail.com>
2020-10-06 16:50:22 -0300

    Projeto iniciado, mostrando a mensagem Ola,Mundo

---
+++ projetoteste/Program.cs
@@ -0,0 +1,14 @@
+﻿using System;
+
+namespace projetoteste
+{
+    class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+
+            Console.ReadLine();
+        }
+    }
+}
