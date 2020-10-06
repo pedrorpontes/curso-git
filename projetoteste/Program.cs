597a448b5128347ba574c72779408d935c2221ae
Pedro Pontes <pedrorpontes@gmail.com>
2020-10-06 17:21:26 -0300

    Acrescentada pergunta sobre como voce esta

--- projetoteste/Program.cs
+++ projetoteste/Program.cs
@@ -8,6 +8,8 @@ namespace projetoteste
         {
             Console.WriteLine("Hello World!");
             Console.WriteLine("Bom dia!");
+            Console.WriteLine("Como você está hoje?");
+
             Console.ReadLine();
             
         }
