3f74ec55af52a61e0acfa47da90f4d337cfc92f6
Pedro Pontes <pedrorpontes@gmail.com>
2020-10-06 17:08:31 -0300

    Acrescentada uma mensagem de bom dia

--- projetoteste/Program.cs
+++ projetoteste/Program.cs
@@ -7,8 +7,9 @@ namespace projetoteste
         static void Main(string[] args)
         {
             Console.WriteLine("Hello World!");
-
+            Console.WriteLine("Bom dia!");
             Console.ReadLine();
+            
         }
     }
 }
